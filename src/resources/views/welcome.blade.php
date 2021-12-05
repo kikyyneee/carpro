d526a261d3a821b96f896db13666fe3bc69b0945
Febrylasena <lasenafebry@gmail.com>
2021-12-05 22:40:58 +0700

    add image holder

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -10,19 +10,19 @@
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="{{asset('styles.css')}}" rel="stylesheet" />
+        <link href="{{asset('style-page.css')}}" rel="stylesheet" />
     </head>
     <body>
         <!-- Responsive navbar-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container px-5">
-                <a class="navbar-brand" href="#!">Start Bootstrap</a>
+                <a class="navbar-brand" href="#!">Luky Mobil</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Services</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Kontak</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Pesan</a></li>
                     </ul>
                 </div>
             </div>
@@ -35,40 +35,52 @@
                 <div class="col-lg-5">
                     <h1 class="font-weight-light">Beli Mobil Baru Mu Disini</h1>
                     <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
-                    <a class="btn btn-primary" href="#!">Call to Action!</a>
+                    <a class="btn btn-primary" href="#!">Pesan</a>
                 </div>
             </div>
             <!-- Call to Action-->
             <div class="card text-white bg-secondary my-5 py-4 text-center">
-                <div class="card-body"><p class="text-white m-0">Pilih Kategori Mobil Yang Kamu Inginkan !</p></div>
+                <div class="card-body"><p class="text-white m-0">Pilih Mobil Impian mu</p></div>
             </div>
             <!-- Content Row-->
             <div class="row gx-4 gx-lg-5">
                 <div class="col-md-4 mb-5">
                     <div class="card h-100">
-                        <div class="card-body">
-                            <h2 class="card-title">Card One</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+                        <div class="image-holder" style="background-image: url({{asset('img/car2.jpg')}});" >
                         </div>
-                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+                        <div class="card-body">
+                            <h2 class="card-title">Mobil 1</h2>
+                            <p class="card-text price">RP.100.000.000</p>
+                            <p class="card-text tenor">2jt an per bulan</p>
+                            <p class="card-text">DP mulai dari 30jt an</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">Pesan disini</a></div>
                     </div>
                 </div>
                 <div class="col-md-4 mb-5">
                     <div class="card h-100">
-                        <div class="card-body">
-                            <h2 class="card-title">Card Two</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos eligendi labore.</p>
+                        <div class="image-holder" style="background-image: url({{asset('img/car1.jpg')}});" >
                         </div>
-                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+                        <div class="card-body">
+                            <h2 class="card-title">Mobil 1</h2>
+                            <p class="card-text price">RP.100.000.000</p>
+                            <p class="card-text tenor">2jt an per bulan</p>
+                            <p class="card-text">DP mulai dari 30jt an</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">Pesan disini</a></div>
                     </div>
                 </div>
                 <div class="col-md-4 mb-5">
                     <div class="card h-100">
-                        <div class="card-body">
-                            <h2 class="card-title">Card Three</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+                        <div class="image-holder" style="background-image: url({{asset('img/car3.jpg')}});" >
                         </div>
-                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+                        <div class="card-body">
+                            <h2 class="card-title">Mobil 1</h2>
+                            <p class="card-text price">RP.100.000.000</p>
+                            <p class="card-text tenor">2jt an per bulan</p>
+                            <p class="card-text">DP mulai dari 30jt an</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">Pesan disini</a></div>
                     </div>
                 </div>
             </div>
